a6601132aa5d12da83df1e40b100e30ea52eff0d
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-01-10 03:45:26 +0100

    Rename edit route

--- src/router/edit.js
+++ src/router/edit.js
@@ -2,6 +2,6 @@ import EditStudent from '@/components/EditStudent';
 
 export default {
   path: '/student/edit/:student_id',
-  name: 'Edit Student',
+  name: 'EditStudent',
   component: EditStudent
 };
